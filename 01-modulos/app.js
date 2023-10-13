c86aa80044b479c8858d386e72ca05f9292117be
Lufeltz <lufelt@proton.me>
2023-10-12 23:45:23 -0300

    chore: Mover diretório 01-modulos para o repositório nodeJs-RalfLima

--- 01-modulos/app.js
+++
@@ -1,8 +0,0 @@
-// importação dos métodos
-const calculos = require("./calculos");
-
-const multiplicar = calculos.multiplicar;
-const somar = calculos.somar;
-
-console.log(multiplicar(10,15));
-console.log(somar(10,15));
