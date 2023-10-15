eaf6fbb669685a913ac12d038c0eca191826aa90
Lufeltz <lufelt@proton.me>
2023-10-14 23:47:57 -0300

    feat: Implementar caminho do diretório estático public

--- nodeJs-RalfLima/04-html/app.js
+++ nodeJs-RalfLima/04-html/app.js
@@ -4,6 +4,9 @@ const express = require("express");
 // App
 const app = express();
 
+// Especificar caminho do diretório public
+app.use(express.static(__dirname + '/public'))
+
 // Rota
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/pagina.html");
