5af1813ccfbf6c0aaf08f0949ee9505e650bace7
Lufeltz <lufelt@proton.me>
2023-10-28 21:24:18 -0300

    chore: Modificar a leitura e escrita nos arquivos para ser async

--- nodeJs-RalfLima/09-projeto/backend/app.js
+++ nodeJs-RalfLima/09-projeto/backend/app.js
@@ -1,40 +1,40 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fs from "fs";
+import fs from "fs/promises";
 
 const app = express();
 const PORT = 3001;
 
 let nextId = 1;
-let clientes = [];
+let clients = [];
 
 app.use(bodyParser.json());
 
-fs.readFile("clientes.json", (err, data) => {
-    if (err) {
-        console.error("Não foi possível ler o conteúdo do arquivo: ", err);
+async function loadClientsData() {
+    try {
+        const data = await fs.readFile("clientes.json");
+        const jsonContent = JSON.parse(data);
+        clients = jsonContent.clients;
+        nextId =
+            clients.length > 0
+                ? Math.max(...clients.map((client) => client.id))
+                : nextId;
+    } catch (error) {
+        console.error("Erro ao ler o arquivo: ", error);
+        throw error;
     }
+}
 
-    if (!data) {
-        console.error("Conteúdo do arquivo nulo ou indefinido.");
-    }
-
-    const jsonContent = JSON.parse(data);
-    clientes = jsonContent.clientes;
-    nextId =
-        clientes.length > 0
-            ? Math.max(...clientes.map((cliente) => cliente.id))
-            : nextId;
-
-    app.listen(PORT, (req, res) => {
-      console.log(`Servidor ouvindo na porta: ${PORT}`);
-    })
+loadClientsData().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta: ${PORT}`);
+    });
 });
-
 
 // Retornar todos os clientes
-app.get("/clientes", (req, res) => {
-    const jsonClientes = JSON.stringify(clientes, null, 2);
-    console.log(jsonClientes);
-    res.status(200).send(jsonClientes);
+app.get("/clients", (req, res) => {
+    const jsonClient = JSON.stringify(clients, null, 2);
+    console.log(jsonClient);
+    res.status(200).send(jsonClient);
 });
+
