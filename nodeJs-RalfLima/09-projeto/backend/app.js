5cbac2cb426552ca565353053a5ec3cb8995c3d2
Lufeltz <lufelt@proton.me>
2023-10-28 21:26:29 -0300

    feat: Implementar rota /clients para inserir novos usuários conforme chamada da rota /register no frontend

--- nodeJs-RalfLima/09-projeto/backend/app.js
+++ nodeJs-RalfLima/09-projeto/backend/app.js
@@ -38,3 +38,41 @@ app.get("/clients", (req, res) => {
     res.status(200).send(jsonClient);
 });
 
+app.post("/clients", async (req, res) => {
+    const newClientRegistration = {
+        nome: req.body.nome,
+        idade: req.body.idade,
+        id: ++nextId,
+    };
+
+    try {
+        const data = await fs.readFile("clientes.json");
+        const jsonContent = JSON.parse(data);
+        jsonContent.clients.push(newClientRegistration);
+
+        await fs.writeFile(
+            "clientes.json",
+            JSON.stringify(jsonContent, null, 2)
+        );
+        clients.push(newClientRegistration);
+
+        res.status(200).json(newClientRegistration);
+    } catch (error) {
+        console.error("Erro ao manipular o arquivo");
+        res.status(500).json({ error: "Erro interno no servidor." });
+    }
+});
+
+// Retornar um cliente especifico
+app.get("/client/:id", (req, res) => {
+    const clientId = req.params.id;
+    const client = clients.find((client) => client.id === parseInt(clientId));
+
+    if (!client) {
+        res.status(404).send("Cliente não encontrado!");
+    } else {
+        const jsonClient = JSON.stringify(client, null, 2);
+        console.log(jsonClient);
+        res.status(200).send(jsonClient);
+    }
+});
