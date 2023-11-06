7f9a6633becb59dab6176108a91280c3b98e10f0
Lufeltz <lufelt@proton.me>
2023-11-05 23:44:09 -0300

    chore: Importar componente Ex03_useState

--- React-Hooks/conceitos/src/main.jsx
+++ React-Hooks/conceitos/src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Ex01 from "./hooks/Ex01_useState";
 import Ex02 from "./hooks/Ex02_useState";
+import Ex03 from "./hooks/Ex03_useState";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Ex02 />
+        <Ex03 />
     </React.StrictMode>
 );
