caed027d3ee262a097ea847789ffb0b0498bb316
Lufeltz <lufelt@proton.me>
2023-11-06 00:40:54 -0300

    chore: Importar Ex04_useState

--- React-Hooks/conceitos/src/main.jsx
+++ React-Hooks/conceitos/src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Ex01 from "./hooks/Ex01_useState";
 import Ex02 from "./hooks/Ex02_useState";
 import Ex03 from "./hooks/Ex03_useState";
+import Ex04 from "./hooks/Ex04_useState";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Ex03 />
+        <Ex04 />
     </React.StrictMode>
 );
