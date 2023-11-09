b39c9413062d69d92545d3287a0e3e40ed6b35f0
Lufeltz <lufelt@proton.me>
2023-11-09 00:47:08 -0300

    chore: Importar Ex05_useEffect

--- React-Hooks/conceitos/src/main.jsx
+++ React-Hooks/conceitos/src/main.jsx
@@ -4,9 +4,10 @@ import Ex01 from "./hooks/Ex01_useState";
 import Ex02 from "./hooks/Ex02_useState";
 import Ex03 from "./hooks/Ex03_useState";
 import Ex04 from "./hooks/Ex04_useState";
+import Ex05 from "./hooks/Ex05_useEffect";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Ex04 />
+        <Ex05 />
     </React.StrictMode>
 );
