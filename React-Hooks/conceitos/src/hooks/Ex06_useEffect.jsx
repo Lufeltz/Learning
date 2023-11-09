8203afa7ac9eb0ca9363453d031da6040fb4d8a5
Lufeltz <lufelt@proton.me>
2023-11-09 01:07:36 -0300

    feat: Adicionar Ex06_useEffect

---
+++ React-Hooks/conceitos/src/hooks/Ex06_useEffect.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+function Ex06_useEffect() {
+    const [vetor, setVetor] = useState([]);
+    const [status, setStatus] = useState("Loading photos...");
+
+    useEffect(() => {
+        getPhotos();
+    });
+
+    const getPhotos = async () => {
+        const photos = await fetch(
+            "https://jsonplaceholder.typicode.com/photos"
+        );
+        const photosJson = await photos.json();
+
+        setVetor(photosJson);
+        setStatus("Photos Loaded!")
+    };
+
+    return (
+        <div>
+          <h1>{status}</h1>
+            {vetor.map((photo) => (
+                <li key={photo.id}>{photo.title}</li>
+            ))}
+        </div>
+    );
+}
+
+export default Ex06_useEffect;
