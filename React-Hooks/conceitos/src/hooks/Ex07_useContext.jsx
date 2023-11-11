02b8efd75fe7954631cc1df037bba363cdb4e485
Lufeltz <lufelt@proton.me>
2023-11-11 14:47:42 -0300

    feat: Utilizar o createContext e useContext

--- React-Hooks/conceitos/src/hooks/Ex07_useContext.jsx
+++ React-Hooks/conceitos/src/hooks/Ex07_useContext.jsx
@@ -1,34 +1,61 @@
-import React from "react";
+import React, { createContext, useContext } from "react";
+
+const context = createContext();
 
 function Principal() {
+    let texto = "Passando a prop!";
 
-    let texto = 'Passando a prop!';
-
-    return (<Camada1 texto={texto}/>);
+    return (
+        <context.Provider value={{ texto }}>
+            <Camada1 />
+        </context.Provider>
+    );
 }
 
-function Camada1({texto}) {
+function Camada1() {
     return (
         <div
             style={{
                 backgroundColor: "red",
                 width: "500px",
                 height: "500px",
-                display:'inline-block'
+                display: "inline-block",
             }}
-        ><Camada2 texto={texto}/></div>
+        >
+            <Camada2 />
+        </div>
     );
 }
 
-function Camada2({texto}){
-  return (
-    <div style={{backgroundColor:'green', width:'400px', height:'400px', margin:'50px', display:'inline-block'}}><Camada3 texto={texto}/></div>
-  )
+function Camada2() {
+    return (
+        <div
+            style={{
+                backgroundColor: "green",
+                width: "400px",
+                height: "400px",
+                margin: "50px",
+                display: "inline-block",
+            }}
+        >
+            <Camada3 />
+        </div>
+    );
 }
 
-function Camada3({texto}){
-  return (
-    <div style={{backgroundColor:'yellow', width: '300px', height:'300px', margin:'50px'}}>{texto}</div>
-  )
+function Camada3() {
+    const { texto } = useContext(context);
+    return (
+        <div
+            style={{
+                backgroundColor: "yellow",
+                width: "300px",
+                height: "300px",
+                margin: "50px",
+            }}
+        >
+            {texto}
+        </div>
+    );
 }
 export default Principal;
