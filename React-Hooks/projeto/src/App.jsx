735106271684259da5d3b976d0df654b6dd5ff0e
Lufeltz <lufelt@proton.me>
2023-11-13 15:20:30 -0300

    feat: Implementar estados dos campos, name, age e city e passá-los ao componente Form

--- React-Hooks/projeto/src/App.jsx
+++ React-Hooks/projeto/src/App.jsx
@@ -1,9 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Form from "./Form";
+import Table from "./Table";
 
 function App() {
-    return <Form />;
+    const [btnRegister, setBtnRegister] = useState(true);
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [city, setCity] = useState("");
+
+    return (
+        <>
+            <Form
+                isRegister={btnRegister}
+                setName={setName}
+                setAge={setAge}
+                setCity={setCity}
+            />
+            <Table />
+        </>
+    );
 }
 
 export default App;
