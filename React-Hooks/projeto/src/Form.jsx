50da634faedf23b7dc922ceabd6a3eab2c4efffa
Lufeltz <lufelt@proton.me>
2023-11-15 23:55:57 -0300

    feat: Resetar os valores dos campos de input

--- React-Hooks/projeto/src/Form.jsx
+++ React-Hooks/projeto/src/Form.jsx
@@ -1,28 +1,29 @@
-function Form({ isRegister, setName, setAge, setCity, register }) {
-
-
+function Form({ isRegister, setName, setAge, setCity, register, user }) {
     return (
         <form>
             <input
                 type="text"
                 name="name"
+                value={user.name}
                 placeholder="Nome"
                 className="form-control"
-                onChange={e => setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
             />
             <input
                 type="text"
                 name="age"
+                value={user.age}
                 placeholder="Idade"
                 className="form-control"
-                onChange={e => setAge(e.target.value)}
+                onChange={(e) => setAge(e.target.value)}
             />
             <input
                 type="text"
                 name="city"
+                value={user.city}
                 placeholder="Cidade"
                 className="form-control"
-                onChange={e => setCity(e.target.value)}
+                onChange={(e) => setCity(e.target.value)}
             />
 
             <div className="btn-flux">
