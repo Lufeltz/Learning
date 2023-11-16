28d7d84a4f95ded45d743b7d9de44720b954244b
Lufeltz <lufelt@proton.me>
2023-11-15 23:32:31 -0300

    feat: Adicionar listagem de usuários

--- React-Hooks/projeto/src/Table.jsx
+++ React-Hooks/projeto/src/Table.jsx
@@ -8,18 +8,23 @@ function Table({ users }) {
                     <th>Nome</th>
                     <th>Idade</th>
                     <th>Cidade</th>
+                    <th>Selecionar</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    {/* {users.map((user) => {
-                        <tr>
-                            <td>{user.name}</td>
-                            <td>{user.age}</td>
-                            <td>{user.city}</td>
-                        </tr>;
-                    })} */}
-                </tr>
+                {users.map((user, indice) => (
+                    <tr key={indice}>
+                        <td>{indice + 1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>{user.city}</td>
+                        <td>
+                            <button className="btn btn-success">
+                                Selecionar
+                            </button>
+                        </td>
+                    </tr>
+                ))}
             </tbody>
         </table>
     );
