9afe502819f7d01459c664fbb7b61d182e1d293b
Lufeltz <lufelt@proton.me>
2023-11-16 23:55:26 -0300

    feat: Adicionar campo de pesquisa para pesquisar usuários pelos seus nomes

--- React-Hooks/projeto/src/Table.jsx
+++ React-Hooks/projeto/src/Table.jsx
@@ -1,32 +1,48 @@
+import { useState } from "react";
+
 // Componente
 function Table({ users, select }) {
+    const [search, setSearch] = useState("");
+
     return (
-        <table className="table">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Nome</th>
-                    <th>Idade</th>
-                    <th>Cidade</th>
-                    <th>Selecionar</th>
-                </tr>
-            </thead>
-            <tbody>
-                {users.map((user, indice) => (
-                    <tr key={indice}>
-                        <td>{indice + 1}</td>
-                        <td>{user.name}</td>
-                        <td>{user.age}</td>
-                        <td>{user.city}</td>
-                        <td>
-                            <button className="btn btn-success" onClick={() => select(indice)}>
-                                Selecionar
-                            </button>
-                        </td>
+        <div>
+            <input
+                type="search"
+                onChange={(e) => setSearch(e.target.value)}
+                placeholder="Informe um nome"
+                className="form-control search"
+            />
+            <p>{search}</p>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Cidade</th>
+                        <th>Selecionar</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {users.filter(user => user.name.toLowerCase().includes(search)).map((user, indice) => (
+                        <tr key={indice}>
+                            <td>{indice + 1}</td>
+                            <td>{user.name}</td>
+                            <td>{user.age}</td>
+                            <td>{user.city}</td>
+                            <td>
+                                <button
+                                    className="btn btn-success"
+                                    onClick={() => select(indice)}
+                                >
+                                    Selecionar
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
 
