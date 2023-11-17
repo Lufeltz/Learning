d5af39896825bb96a00e74921173b10aba931f77
Lufeltz <lufelt@proton.me>
2023-11-17 00:07:52 -0300

    chore: Criara função filteredUsers para lidar com a pesquisa de usuários e facilitar legibilidade

--- React-Hooks/projeto/src/Table.jsx
+++ React-Hooks/projeto/src/Table.jsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 // Componente
 function Table({ users, select }) {
     const [search, setSearch] = useState("");
+    const filteredUsers = (name, nameSearched) =>
+        name.toLocaleLowerCase().includes(nameSearched.toLocaleLowerCase());
 
     return (
         <div>
@@ -12,7 +14,6 @@ function Table({ users, select }) {
                 placeholder="Informe um nome"
                 className="form-control search"
             />
-            <p>{search}</p>
             <table className="table">
                 <thead>
                     <tr>
@@ -24,22 +25,24 @@ function Table({ users, select }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.filter(user => user.name.toLowerCase().includes(search)).map((user, indice) => (
-                        <tr key={indice}>
-                            <td>{indice + 1}</td>
-                            <td>{user.name}</td>
-                            <td>{user.age}</td>
-                            <td>{user.city}</td>
-                            <td>
-                                <button
-                                    className="btn btn-success"
-                                    onClick={() => select(indice)}
-                                >
-                                    Selecionar
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
+                    {users
+                        .filter((user) => filteredUsers(user.name, search))
+                        .map((user, indice) => (
+                            <tr key={indice}>
+                                <td>{indice + 1}</td>
+                                <td>{user.name}</td>
+                                <td>{user.age}</td>
+                                <td>{user.city}</td>
+                                <td>
+                                    <button
+                                        className="btn btn-success"
+                                        onClick={() => select(indice)}
+                                    >
+                                        Selecionar
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
                 </tbody>
             </table>
         </div>
