68b02a40902c963e7565b56118feed3ebb28e687
Lufeltz <lufelt@proton.me>
2023-11-19 00:44:37 -0300

    chore: Adicionar o namespace Models a classe Person

--- API-ASP_NET/Models/Person.cs
+++ API-ASP_NET/Models/Person.cs
@@ -1,10 +1,13 @@
 using System.ComponentModel.DataAnnotations;
 
-public class Person
+namespace Models
 {
-  [Key]
-  public int Id { get; set; }
-  public string Name { get; set; }
-  public string City { get; set; }
-  public int Age { get; set; }
+  public class Person
+  {
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public int Age { get; set; }
+  }
 }
